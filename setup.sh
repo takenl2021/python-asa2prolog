e7da8d6a70b929d54db64f11463a83c4391d35d9
a-msy <m5s2y5a@gmail.com>
2021-05-29 15:54:33 +0900

    ignore 反映

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 git clone https://github.com/takenl2021/python_asa
-cd python_asa
+cd ./python_asa
 pip install -e .
 cd ../
 git clone https://github.com/yuce/pyswip.git
-cd pyswip
+cd ./pyswip
 pip install -e .
 cd ../
 pip install fastapi
